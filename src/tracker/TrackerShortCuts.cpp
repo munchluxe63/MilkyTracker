1c9f1d9c955884152cd1630c7925e56e0ec6d020
Dale Whinham <dale.whinham@arm.com>
2016-07-25 14:30:48 +0100

    fixup! PatternEditor: Use eKeyChar for data input events

--- src/tracker/TrackerShortCuts.cpp
+++ src/tracker/TrackerShortCuts.cpp
@@ -626,15 +626,16 @@ static void simulateMouseClickEvent(PPControl* ctrl)
 bool Tracker::processMessageBoxShortcuts(PPEvent* event)
 {
 	PPControl* ctrl = screen->getModalControl();
-	
-	if (ctrl == NULL || !ctrl->isContainer() || event->getID() != eKeyDown)
+
+	if (ctrl == NULL || !ctrl->isContainer() || (event->getID() != eKeyDown
+			&& event->getID() != eKeyChar))
 		return false;
 
 	PPSimpleVector<PPControl>& controls = static_cast<PPContainer*>(ctrl)->getControls();
 
 	pp_int32 i;
 	// if dialog contains list (list can also be an edit field btw.)
-	// and something is being edited in that list we don't simulate 
+	// and something is being edited in that list we don't simulate
 	// yes/no/cancel button presses
 	for (i = 0; i < controls.size(); i++)
 	{
@@ -643,34 +644,37 @@ bool Tracker::processMessageBoxShortcuts(PPEvent* event)
 			return true;
 	}
 
-	// iterate over controls in dialog and see whether we can find 
+	// iterate over controls in dialog and see whether we can find
 	// yes/no/cancel buttons
 	// if that's the case we simulate mouse button press
-	pp_uint16 keyCode = *((pp_uint16*)event->getDataPtr());
-
-	for (i = 0; i < controls.size(); i++)
+	if (event->getID() == eKeyDown)
 	{
-		PPControl* ctrl = controls.get(i);
-		switch (ctrl->getID())
+		pp_uint16 keyCode = *((pp_uint16*)event->getDataPtr());
+
+		for (i = 0; i < controls.size(); i++)
 		{
-			case PP_MESSAGEBOX_BUTTON_YES:	
-				if (keyCode == VK_RETURN)
-				{
-					simulateMouseClickEvent(ctrl);
-					return true;
-				}
-				break;
-				
-			case PP_MESSAGEBOX_BUTTON_CANCEL:
-			case PP_MESSAGEBOX_BUTTON_NO:
-				if (keyCode == VK_ESCAPE)
-				{
-					simulateMouseClickEvent(ctrl);
-					return true;
-				}
-				break;
+			PPControl* ctrl = controls.get(i);
+			switch (ctrl->getID())
+			{
+				case PP_MESSAGEBOX_BUTTON_YES:
+					if (keyCode == VK_RETURN)
+					{
+						simulateMouseClickEvent(ctrl);
+						return true;
+					}
+					break;
+
+				case PP_MESSAGEBOX_BUTTON_CANCEL:
+				case PP_MESSAGEBOX_BUTTON_NO:
+					if (keyCode == VK_ESCAPE)
+					{
+						simulateMouseClickEvent(ctrl);
+						return true;
+					}
+					break;
+			}
 		}
 	}
-	
+
 	return false;
 }
